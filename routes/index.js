3bdffd2d86d544db41447a0114896b31a58f9f19
Teddy10000 <tedakrong12@gmail.com>
2023-12-03 20:56:26 +0000

    did this

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
-const AuthController = require('../controllers/AuthController');
+const AuthController = require('../controllers/AuthController'); 
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -12,6 +13,13 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', AuthController.getMe);
+router.get('/users/me', AuthController.getMe); 
+
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+router.get('/files', xTokenAuthenticate, FilesController.getIndex);
+router.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+router.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
